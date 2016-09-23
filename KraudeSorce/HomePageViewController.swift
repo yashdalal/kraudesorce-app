b1bb76f62342ed053379beb4f23c857ff341357e
Yash Dalal <yashdalal95@gmail.com>
2016-09-23 18:25:34 -0500

    screen turns green when you enter holmes, updates counter, reverse when you leave

--- KraudeSorce/HomePageViewController.swift
+++ KraudeSorce/HomePageViewController.swift
@@ -8,10 +8,14 @@
 
 import UIKit
 import CoreLocation
+import Firebase
 
 class HomePageViewController: UIViewController, CLLocationManagerDelegate {
     
     let locationManager = CLLocationManager()
+    var user : User!
+    let ref = FIRDatabase.database().referenceWithPath("count")
+//    var holmesRegion: CLRegion!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +23,13 @@ class HomePageViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.requestAlwaysAuthorization()
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.startUpdatingLocation()
-        // Do any additional setup after loading the view.
+        let holmesRegion = CLCircularRegion(center: CLLocationCoordinate2D(latitude: 38.648155,longitude: -90.306414), radius: 200, identifier: "holmes")
+        locationManager.startMonitoringForRegion(holmesRegion)
+        
+        FIRAuth.auth()?.addAuthStateDidChangeListener{ auth, user in
+            guard let user = user else { return }
+            self.user = User(authData: user)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,15 +37,35 @@ class HomePageViewController: UIViewController, CLLocationManagerDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    func locationManager(manager: CLLocationManager,
-                                    didUpdateLocations locations: [CLLocation]){
-        print(locations)
-    }
+//    func locationManager(manager: CLLocationManager,
+//                                    didUpdateLocations locations: [CLLocation]){
+//        let currentLocation = locations[0]
+//        let lat = currentLocation.coordinate.latitude
+//        let long = currentLocation.coordinate.longitude
+//        let inHolmes = self.isInHolmes(lat, longitude: long)
+//        print(inHolmes)
+//    }
     
-    
+//    func isInHolmes(latitude: CLLocationDegrees, longitude: CLLocationDegrees) -> Bool {
+//        return (latitude > 38) && (latitude < 39) && (longitude < (-90)) && (longitude > (-91))
+//    }
     
     func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
-        <#code#>
+        print(error)
+    }
+    
+    func locationManager(manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        self.view.backgroundColor = UIColor.greenColor()
+        ref.setValue(1)
+    }
+    
+    func locationManager(manager: CLLocationManager, didExitRegion region: CLRegion) {
+        self.view.backgroundColor = UIColor.redColor()
+        ref.setValue(0)
+    }
+    
+    func locationManager(manager: CLLocationManager, monitoringDidFailForRegion region: CLRegion?, withError error: NSError) {
+        print("\(error)")
     }
 
     /*
---
+++ KraudeSorce/User.swift
@@ -0,0 +1,25 @@
+//
+//  User.swift
+//  KraudeSorce
+//
+//  Created by Yash Dalal on 9/23/16.
+//  Copyright © 2016 Yash Dalal. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class User {
+    let uid : String
+    let email : String
+    
+    init(authData: FIRUser){
+        uid = authData.uid
+        email = authData.email!
+    }
+    
+    init(uid: String, email: String){
+        self.uid = uid
+        self.email = email
+    }
+}
